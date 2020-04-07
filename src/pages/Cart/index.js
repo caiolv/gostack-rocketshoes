2b2208ba3ea5545ede433c62e13ec28b99887b77
Caio Oliveira <caiolv@me.com>
2020-04-07 17:37:22 -0300

    Estilização do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return (<h1>Cart</h1>);
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tênis exemplo</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" value={2} readOnly />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>RS 259,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,20</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
